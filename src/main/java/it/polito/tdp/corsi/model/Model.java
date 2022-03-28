9ac5e27736a664de4c605c6251b841dab346ae2d
Leti <Leti@LAPTOP-U6R8VABT.lan>
2022-03-29 00:12:09 +0200

    Versione lezione 23/03/2022

--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,7 +1,21 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.List;
+
+import it.polito.tdp.corsi.db.CorsoDAO;
 
 public class Model {
+	//DAO richiamato dal model e model richiamato dal controller
+	private CorsoDAO corsoDao;
 	
+	
+	public Model(){
+		this.corsoDao = new CorsoDAO();
+	}
+
+
+	public List<Corso> getCorsoByPeriodo(int periodo){
+		return this.corsoDao.getCorsoByPeriodo(periodo);
+	}
 	
 }
